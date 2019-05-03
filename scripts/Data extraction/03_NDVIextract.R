1416219e459247600628b7f4d15918f1a240af4c
Emilie Dedeban <ededeban@mun.ca>
2019-05-03 10:26:29 -0230

    NDVI script added

---
+++ scripts/Data extraction/03_NDVIextract.R
@@ -0,0 +1,91 @@
+###~~~Extraction of NDVI data~~~###
+
+#########  Extracting spatial data from rasters to points  ##############
+library(raster)
+libs <- c('data.table', 'rgdal')
+caribouData<-readRDS('~/Stage/Stage Canada/Caribou data/cleaned-locs.Rds')  
+
+
+### remove 276 Julian day 
+caribouData <- caribouData[JDate >= 50 & JDate <= 275]
+##### Now to extract NDVI, which has a unique layer for each day:
+
+#### Subset down to only one year (this step needs to be done one year at a time)
+
+Data2006<-subset(caribouData, year=="2006")
+
+#### I first give it a point ID, to be able to re-sort it again later
+Data2006$ptID<-c(1:nrow(Data2006))
+
+#### Now I order the dataframe by Julian Day
+DataSort2006<-Data2006[order(Data2006$JDate),]
+
+#### Set the working directory to a folder with all the NDVI files for that year
+#### (doesn't seem to work without setting the WD)
+setwd('E:/NDVI/DailyNDVI_IRG/2013')
+setwd('C:/Users/emitn/OneDrive/Documents/Stage/Stage Canada/NDVI/DailyNDVI_IRG/2006')
+
+#### Use List files to get the names of the files
+Files2013<-list.files('E:/NDVI/DailyNDVI_IRG/2013')
+Files2006<-list.files('C:/Users/emitn/OneDrive/Documents/Stage/Stage Canada/NDVI/DailyNDVI_IRG/2006')
+
+#### This then iterates the "raster" function to read in and make
+#### a raster object for each day
+NDVIrasters2006<-sapply(Files2006, raster)
+
+#### Here's the loop to extract the data for each day. It subsets the data to only include day i,
+#### and extracts the value of the raster which represents the data for day i.
+
+#### This generates a vector of NDVI values for each day ("NDVI2013070", "NDVI2013071", "NDVI2013072", etc)
+#### The first loop only loops over data until day 99
+for (i in min(DataSort2006$JDate):99){
+  assign(paste("NDVI2006",i,sep=""),extract(x=eval(parse(text=paste("NDVIrasters2006$Pred20060",i,".tif",sep=""))),
+                                            y=data.frame(subset(DataSort2006,JDate==i)$EASTING,
+                                                         subset(DataSort2006,JDate==i)$NORTHING)))
+}
+
+#### This code is the same but for day 100 +
+#### These are different because the file names before 100 have an extra 0 before them so
+#### they won't match up with i (e.g., 71 versus 071). That's why the code above is "Pred20130"
+#### and the one below is "Pred2013"
+
+for (i in 100:max(DataSort2006$JDate)){
+  print(i)
+  assign(paste("NDVI2006",i,sep=""),extract(x=eval(parse(text=paste("NDVIrasters2006$Pred2006",i,".tif",sep=""))),
+                                            y=data.frame(subset(DataSort2006,JDate==i)$EASTING,
+                                                         subset(DataSort2006,JDate==i)$NORTHING)))
+}
+
+
+#### This code takes all the individual vectors produced above and joins them into a
+#### single vector. The first run of the loop creates an object with the first vector
+#### (NDVIval2013), then all future times through the loop it combines the result
+#### of the previous loop with the data from the next day:
+
+t <- 1    
+for(i in min(DataSort2006$JDate):max(DataSort2006$JDate)){
+  if (t==1){
+    NDVIval2006 <- eval(parse(text=paste("NDVI2006",i,sep="")))
+  } else {
+    NDVIval2006 <- c(NDVIval2006, eval(parse(text=paste("NDVI2006",i,sep=""))))
+  }
+  t <- t+1
+}
+
+
+#### Then this vector can be re-attached to the original dataframe as the vector
+#### of NDVI values
+DataSort2006$NDVI<-NDVIval2006
+
+#### This can then be resorted by the point ID created earlier to put the data
+#### back into the order it was in previously:
+DataReSort2006<-DataSort2006[order(DataSort2006$ptID),]
+
+
+saveRDS(DataReSort2006, '~/Stage/Stage Canada/Caribou data/DataReSort2006.Rds')
+save(DataReSort2006, file="~/Stage/Stage Canada/Caribou data/DataReSort2006.csv")
+
+##Calculate number of herd that I have by year (!!Midridge!! and year 2006!!)
+
+length(unique(DataReSort2006$HERD))
+
