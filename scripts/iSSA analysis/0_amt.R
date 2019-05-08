b7ae5753239c137864e825afd5160ae7c454cc5e
Emilie Dedeban <ededeban@mun.ca>
2019-05-08 19:40:47 -0230

    Script updated

--- scripts/iSSA analysis/0_amt.R
+++ scripts/iSSA analysis/0_amt.R
@@ -8,8 +8,6 @@ library(tidyverse) # not in paper
 library(ggplot2)
 library(parallel)
 library(data.table)
-library(rgdal)
-library(dplyr)
 
 ####test with only 6 individuals
 dat <- readRDS("~/Git/emilie_nlcaribou/output/Data extraction/cariboumodel.Rds") %>% 
@@ -17,6 +15,13 @@ dat <- readRDS("~/Git/emilie_nlcaribou/output/Data extraction/cariboumodel.Rds")
   select(x = "EASTING", y = "NORTHING",
          t = "datetime", id = "ANIMAL_ID") %>% 
   filter(id %in% c("sc2006019", "sc2007036", "sc2007038", "mr2009a09", "mr2009a02", "sc2007090"))    # for example 2
+
+
+test2<-dat    
+#####Change formats 
+test2$id<-as.character(test2$id)
+test2$t<- as.POSIXct(test2$t, format = "%Y-%m-%d %H:%M:%OS")       ######After this, 7 NA appears in the column t
+test2<-na.omit(test2, cols = "t", invert = FALSE)    
 
 ####split data#############################
 test2$Date<-as.Date(test2$t)
@@ -27,19 +32,16 @@ do.call(rbind, out)
 head(out)
 data.frame(test2$new, do.call(rbind, out))
 ###########################################
-test2<-dat
-
-test2$id<-as.character(test2$id)
-test2$t<- as.POSIXct(test2$t, format = "%Y-%m-%d %H:%M:%OS")
-test2<-na.omit(test2, cols = "t", invert = FALSE)
 
 test2 <- test2 %>% nest(-id)
-
 test2 <- test2 %>%
   mutate(trk = lapply(data, function(d) {
     mk_track(d, x, y, t, crs = sp::CRS("+init=epsg:4326")) %>%
       transform_coords(sp::CRS("+init=epsg:5070"))
   }))
+
+
+
 
 ##Number of relocations with 12h fix rate
 ###Number of individuals each year, number of herd
