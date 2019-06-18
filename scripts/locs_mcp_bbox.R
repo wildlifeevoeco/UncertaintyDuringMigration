b02f850f0bd47fa6631db3b68843c7baaccd69e1
Alec Robitaille <robit.alec@gmail.com>
2019-06-18 16:09:57 -0700

    fin bbox

--- scripts/locs_mcp_bbox.R
+++ scripts/locs_mcp_bbox.R
@@ -12,6 +12,19 @@ utm21N <- '+proj=utm +zone=21 ellps=WGS84'
 ### Processing ----
 # Convert to a SpatialPointsDataFrame
 spdf <- SpatialPointsDataFrame(
-  locs[, .(X, Y)],
-  proj4string = utm21N
+  coords = locs[, .(X, Y)],
+  proj4string = utm21N,
+  data = locs[, .(ID)]
 )
+
+# Generate MCP
+poly <- mcp(
+  xy = spdf, 
+  percent = 100
+)
+
+# Generate bbox
+box <- bbox(poly)
+
+### Output ----
+saveRDS('output/caribou-bbox.Rds')
