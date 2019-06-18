586e36903736315a26eeb827bf2bb51631fb7f05
Alec Robitaille <robit.alec@gmail.com>
2019-06-18 16:11:27 -0700

    use new bbox in extraction

--- scripts/Data extraction/04_ClimateNetCDF.R
+++ scripts/Data extraction/04_ClimateNetCDF.R
@@ -19,7 +19,7 @@ library(dplyr)
 #setwd(workdir)
 
 # Extent
-bounds <- st_read('input/NL-Bounds/NL-Bounds.shp')
+bounds <- st_bbox(readRDS('output/caribou-bbox.Rds'))
 
 
 daymetEPSG <- '+proj=lcc +datum=WGS84 +lat_1=25 n +lat_2=60n +lat_0=42.5n +lon_0=100w'
@@ -31,7 +31,7 @@ plot(st_transform(bounds, daymetEPSG))
 transBounds <- st_transform(bounds, daymetEPSG)
 
 
-boundsBox <- st_bbox(transBounds)
+boundsBox <- st_bbox(transBounds) 
 
 # set path and filename
 ncname <- "Weather/daymet_v3_tmax_2008_na.nc4"
