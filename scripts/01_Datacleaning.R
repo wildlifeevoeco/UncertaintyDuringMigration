0e1dd3ebaf8b48a144ea018ef2ef7906eb770602
Emilie Dedeban <ededeban@mun.ca>
2019-06-21 22:07:25 -0230

    script

--- scripts/01_Datacleaning.R
+++ scripts/01_Datacleaning.R
@@ -1,6 +1,5 @@
 #### Data cleaning ### 
 rm(list=ls())
-setwd("~/Stage/Stage Canada/Caribou data")
 
 # load packages 
 .libPaths("C:/R") # location of your R packages
@@ -9,61 +8,71 @@ library(tidyr)
 library(moveHMM)
 library(data.table)
 library(rgdal)
+library(toast)
 lapply(libs,require, character.only = TRUE)
+install.packages("toast")
 
 ### read database and csv file ####
-DT <- read_csv("~/Git/emilie_nlcaribou/input/1-CaribouDataSpringClean.CSV")
+caribou <- read_csv("~/Emilie_project/Git/emilie_nlcaribou/input/CaribouDataClean.CSV")
+caribou <- as.data.table(caribou)
 
-##summary of tab
-summary(DT$HERD)
-summary(DT)
-glimpse(DT)
+###1st part : exploration 
+caribou$HERD <- as.factor(caribou$HERD)
 
-##Transform to coord -> UTM
-utm21N <- '+proj=utm +zone=21 ellps=WGS84'
-DT[, c('EASTING', 'NORTHING') := as.data.table(project(cbind(X_COORD, Y_COORD), utm21N))]
-head(DT)
+##subset HERD MIDRIDGE
+summary(caribou$HERD)
+caribouclean <- caribou[HERD == 'MIDRIDGE']
+
+##Subset by Jdate (see Migration dates)
+
 
 ##Add columns with ID,year,Herd
-DT[,ID := paste (ANIMAL_ID, year, HERD, sep = '_')]
+caribouclean[,ID := paste (ANIMAL_ID, year, HERD, sep = '_')]
 
-##Add columns with Year/Herd
-DT[, YearHerd := paste(year, HERD, sep = '_')]
 
 ## Calculate number of locations by year and by herd, should I remove years when I don't have enought of data?
-DT[, Nunique := .N, by = ID]
-DT <- DT[Nunique > 50]
-DT[, .N, by = .(HERD,year)]
+caribouclean[, Nunique := .N, by = ID]
+caribouclean <- caribouclean[Nunique > 50]
+numloc <- caribouclean[, .N, by = .(HERD,year)]
+numloc
+write.csv(numloc, '~/Emilie_project/Git/emilie_nlcaribou/output/numloc.csv')
 
 ##count the number of elements by columns
-sapply(DT, function(x) sum(!duplicated(x)))
-length(unique(caribouclean$ANIMAL_ID))  ###1479
-length(DT$year)
+sapply(caribouclean, function(x) sum(!duplicated(x))) 
+length(unique(caribouclean$ANIMAL_ID))  ###133
 
 #Number of data per year/herd
-DT[, uniqueN(ANIMAL_ID), by = .(HERD,year)] # ind/year/herd
-DT[, .N, by = .(HERD, year, ID)][order(N)] ##nib fixes/nd/year/herd
+numindiv <- caribouclean[, uniqueN(ANIMAL_ID), by = .(HERD,year)] # numind/year/herd Careful, we have more than one year for some indiv
+write.csv(numindiv, '~/Emilie_project/Git/emilie_nlcaribou/output/numindiv.csv')
+
+#caribouclean[, .N, by = .(HERD, year, ID)][order(N)] ##nib fixes/nd/year/herd
                                          
 ##Number of fixes by ind by date
-fixes<-DT[, uniqueN(FIX_TIME), by =.(FIX_DATE, ANIMAL_ID)]
+fixes<-caribouclean[, uniqueN(FIX_TIME), by =.(FIX_DATE, ANIMAL_ID)]
 fixes
 
-#Number of ind / herd
-DT[, uniqueN (ANIMAL_ID), by =.(HERD)]
+##number of day/indiv
+caribouclean[, uniqueN(FIX_DATE), by =.(ANIMAL_ID)]
+
+#Number of dataindiv / herd and number year/indiv
+caribouclean[, uniqueN (ANIMAL_ID), by =.(HERD)]
+caribouclean[, uniqueN(year), by =.(ANIMAL_ID)]
 
 ###remove data that I don't need 
-caribouclean<-subset(cariboudata, select = -c(3,5,11:12,14:23))
+caribouclean<-subset(caribouclean, select = -c(1,6:8, 15:18, 21))
 summary(caribouclean$HERD)
 
+###2nd part : subset by the dates of migration and Midridge
+
+
+
+
+
+
+
+
+
+
+
 ###save Data
-
-saveRDS(DT[, .(V1, ANIMAL_ID, HERD, datetime, year, JDate,
-               EASTING, NORTHING, ID, YearHerd)],
-        '~/Stage/Stage Canada/Caribou data/cleaned-locs.Rds')
-
-saveRDS(fixes[,.(FIX_DATE, ANIMAL_ID, V1)],
-        '~/Stage/Stage Canada/Caribou data/fixes.Rds')
-
-
-
-
+saveRDS(caribouclean, '~/Emilie_project/Git/emilie_nlcaribou/output/caribouclean.Rds')
