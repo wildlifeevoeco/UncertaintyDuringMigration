b32bb05a743bd6993626b0c36d61e348642c825b
Emilie Dedeban <ededeban@mun.ca>
2019-06-24 15:35:09 -0230

    New script to extract MoveHMM states

--- scripts/MoveHMM_MR.R
+++ scripts/MoveHMM_MR.R
@@ -1,32 +1,73 @@
-#### MoveHMM
+#### MoveHMM analysis ###
 
 library(moveHMM)
+library(data.table)
 
-Migr<-readRDS("~/Emilie_project/Git/emilie_nlcaribou/output/Caribouclean.RDS")
+## load data
+caribouclean<-readRDS("~/Emilie_project/Git/emilie_nlcaribou/output/caribouclean.RDS")
 
-MigrMidR2010<-subset(caribouclean, Year==2010 & Herd=="MIDRIDGE")
-MigrMidR2010<-subset(caribouclean, Year==2010)
-unique(MigrMidR2010$Year)
-levels(MigrMidR2010$Herd)
-MigrMidR2010$Herd <- levels(droplevels(MigrMidR2010$Herd))
-unique(MigrMidR2010$Herd)
+# check than all years for only MIDRIDGE is subset and drop others level
+MigrMidR<-subset(caribouclean, Herd=="MIDRIDGE")
+unique(MigrMidR$Year)
+levels(MigrMidR$Herd)
+MigrMidR$Herd <- levels(droplevels(MigrMidR$Herd))
+unique(MigrMidR$Herd)
 
-nrow(MigrMidR2010)
+# Check number indiv == 34
+unique(MigrMidR$Animal_ID)
 
-data<-data.frame(MigrMidR2010$Easting,MigrMidR2010$Northing,MigrMidR2010$IDYear)
+# Check if there is step = 0, if yes -->  add zero mass parameter 
+whichzero <- which(MRPrep$step == 0)
+length (whichzero/nrow(MRPrep))
+
+## Put the dataframe in order --> check year in first, then animal and then the date
+MigrOrdered <- MigrMidR[order(MigrMidR$Year, MigrMidR$Animal_ID, MigrMidR$JDateTime),]
+head(MigrMidR)
+
+##Function 
+data<-data.frame(MigrOrdered$Easting,MigrOrdered$Northing,MigrOrdered$IDYear)
 colnames(data)<-c("x","y","ID")
 
+# Prep data to moveHMM
 MRPrep<-prepData(data, type="UTM")
 
+# Check step and angle graphs
 hist(MRPrep$step)
 hist(MRPrep$angle)
 
 head(MRPrep)
 
-fitHMM<-fitHMM(MRPrep, nbStates=2, stepPar0=c(20,500,100,100), 
+##Fit HMM
+fitHMM1<-fitHMM(MRPrep, nbStates=2, stepPar0=c(20,200,100,100,0.01,0.001), 
                 anglePar0=c(pi,0,1,3), verbose=2)
 
-fitHMM
+fitHMM1
 
-###state 2 = mvt and state 1 = encamp
+### Wrap up states + residuals ----
+# Decode the most probable state sequence - 1 = "encamped" 2 = "exploratory" (viterbi)
+#  + pseudo residuals
+DecodeAndPseudo <- function(x){
+  cbind(state = viterbi(x),
+        x$data,
+        pseudoRes(x))
+}
 
+ls.fits <- list(fitHMM)
+
+collect <- lapply(ls.fits, FUN = function(fit){
+  DecodeAndPseudo(fit)
+})
+
+##Check results on graphs (steplength, turnangle and example of indiv)
+plot(fitHMM,ask=TRUE,animals=NULL,breaks=20)
+
+locs.w.states <- rbindlist(collect)
+
+##number of movement and encamp values
+aggregate(locs.w.states$state, locs.w.states[,c("state")], length)
+summary(locs.w.states)
+
+# Merge the states back onto the locs
+out <- merge(locs, locs.w.states, 
+             by.x = c('EASTING', 'NORTHING', 'ANIMAL_ID'),
+             by.y = c('x', 'y', 'ID'))
