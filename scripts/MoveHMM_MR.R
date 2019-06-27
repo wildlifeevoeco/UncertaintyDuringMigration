60cb8e8aa9015a93b629e8280416db6f3df97a7c
Emilie Dedeban <ededeban@mun.ca>
2019-06-26 22:17:03 -0230

    run with new indiv dates

--- scripts/MoveHMM_MR.R
+++ scripts/MoveHMM_MR.R
@@ -4,7 +4,7 @@ library(moveHMM)
 library(data.table)
 
 ## load data
-caribouclean<-readRDS("~/Emilie_project/Git/emilie_nlcaribou/output/caribouclean.RDS")
+caribouclean<-readRDS("~/Documents/Emilie_project/Git/emilie_nlcaribou/output/cariboucleanMR.RDS")
 
 # check than all years for only MIDRIDGE is subset and drop others level
 MigrMidR<-subset(caribouclean, Herd=="MIDRIDGE")
@@ -15,10 +15,6 @@ unique(MigrMidR$Herd)
 
 # Check number indiv == 34
 unique(MigrMidR$Animal_ID)
-
-# Check if there is step = 0, if yes -->  add zero mass parameter 
-whichzero <- which(MRPrep$step == 0)
-length (whichzero/nrow(MRPrep))
 
 ## Put the dataframe in order --> check year in first, then animal and then the date
 MigrOrdered <- MigrMidR[order(MigrMidR$Year, MigrMidR$Animal_ID, MigrMidR$JDateTime),]
@@ -31,6 +27,10 @@ colnames(data)<-c("x","y","ID")
 # Prep data to moveHMM
 MRPrep<-prepData(data, type="UTM")
 
+# Check if there is step = 0, if yes -->  add zero mass parameter 
+whichzero <- which(MRPrep$step == 0)
+length (whichzero/nrow(MRPrep))
+
 # Check step and angle graphs
 hist(MRPrep$step)
 hist(MRPrep$angle)
@@ -38,11 +38,11 @@ hist(MRPrep$angle)
 head(MRPrep)
 
 ##Fit HMM
-fitHMM1<-fitHMM(MRPrep, nbStates=2, stepPar0=c(20,200,100,100,0.01,0.001), 
+mid.fit<-fitHMM(MRPrep, nbStates=2, stepPar0=c(20,200,100,100,0.01,0.001), 
                 anglePar0=c(pi,0,1,3), verbose=2)
 
-fitHMM1
-saveRDS(fitHMM1, '~/Emilie_project/Git/emilie_nlcaribou/output/fitHMM1.Rds')
+mid.fit
+saveRDS(mid.fit, '~/Documents/Emilie_project/Git/emilie_nlcaribou/output/moveHMM/mid.fit.RDS')
 
 ### Wrap up states + residuals ----
 # Decode the most probable state sequence - 1 = "encamped" 2 = "exploratory" (viterbi)
@@ -53,32 +53,32 @@ DecodeAndPseudo <- function(x){
         pseudoRes(x))
 }
 
-ls.fits <- list(fitHMM1)
+ls.fits <- list(mid.fit)
 
 collect <- lapply(ls.fits, FUN = function(fit){
   DecodeAndPseudo(fit)
 })
 
 ##Check results on graphs (steplength, turnangle and example of indiv)
-plot(fitHMM1,ask=TRUE,animals=NULL,breaks=20)
+plot(mid.fit,ask=TRUE,animals=NULL,breaks=20)
 
 locs.w.states <- rbindlist(collect)
 
 ##number of movement and encamp values
 aggregate(locs.w.states$state, locs.w.states[,c("state")], length)
 #state     x
-# 1     28282
-# 2      9930
+# 1     26384
+# 2      9406
 
 summary(locs.w.states)
 
 # Merge the states back onto the locs
-out <- merge(MigrMidR, locs.w.states, 
+outMR <- merge(MigrMidR, locs.w.states, 
              by.x = c('Easting','Northing','IDYear'),
              by.y = c('x', 'y', 'ID'))
 MigrMidR$ptID<-c(1:nrow(MigrMidR))
 
-out$ptID[duplicated(out$ptID)]
-out<-out[!duplicated(out$ptID), ]
+outMR$ptID[duplicated(outMR$ptID)]
+outMR<-outMR[!duplicated(outMR$ptID), ]
 
-saveRDS(out, '~/Emilie_project/Git/emilie_nlcaribou/output/MoveHMM/out.Rds')
+saveRDS(outMR, '~/Documents/Emilie_project/Git/emilie_nlcaribou/output/MoveHMM/outMR.Rds')
