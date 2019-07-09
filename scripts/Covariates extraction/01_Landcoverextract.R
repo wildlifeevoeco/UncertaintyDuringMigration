80ed9e894d5ddc81270d10298bac1bf0e45d0686
Emilie Dedeban <ededeban@mun.ca>
2019-07-09 10:51:29 -0230

    extract landcover var
    Former-commit-id: 070503c7e163ef46f861bc23cbc4c658197125cc

--- scripts/Covariates extraction/01_Landcoverextract.R
+++ scripts/Covariates extraction/01_Landcoverextract.R
@@ -4,7 +4,8 @@ library(raster)
 library(data.table)
 
 ### Simple example for landcover data (or any other data that is temporally static):
-Filesraster<-list.files('C:/Users/emitn/OneDrive/Documents/Stage/Stage Canada/landcover/tifraster')
+Filesraster<-list.files('~/Documents/Emilie_project/Git/landcover')
+setwd('~/Documents/Emilie_project/Git/landcover')
 
 ### Read in the landcover data using "raster"
 Conifer<-raster("Conifer100.tif")
@@ -16,17 +17,20 @@ Rocky<-raster("Rocky100.tif")
 Water<-raster("Water100.tif")
 Wetland<-raster("Wetland100.tif")
 
+MixedWood_Bdl <- Broadleaf + MixedWood
+Forest <- Broadleaf + MixedWood + Conifer
 
 ### Then use the extract function to get the value of the raster at the points
 ##example : ConiferData<-extract(x=Conifer,y=data.frame(caribouData$UTMX,caribouData$UTMY))
 
-WetlandData<-extract(x=Wetland, y=data.frame(cariboumodel$EASTING,cariboumodel$NORTHING))
+#WetlandData<-extract(x=Wetland, y=data.frame(cariboumodel$EASTING,cariboumodel$NORTHING))
+WaterData<-extract(x=Water, y=data.frame(allweather$Easting,allweather$Northing))
 
 ### This generates a vector of the values at all your locations, 
 ### but it's easiest to just append it to your dataset:
 ##example : caribouData$Conifer<-extract(x=Conifer,y=data.frame(caribouData$UTMX,caribouData$UTMY))
 
-cariboumodel$Wetland<-extract(x=Wetland, y=data.frame(cariboumodel$EASTING,cariboumodel$NORTHING))
+allweather$Water<-extract(x=Water, y=data.frame(allweather$Easting,allweather$Northing))
 
 ### You can do this for each habitat class you're interested in,
 ### or combine categories:
@@ -45,5 +49,5 @@ cariboumodel<-cariboumodel[, ConiferScrub := NULL]
 ###Save data 
 saveRDS(cariboumodel, '~/Git/emilie_nlcaribou/output/Data extraction/cariboumodel.Rds')
 save(cariboumodel, file ='~/Git/emilie_nlcaribou/output/Data extraction/cariboumodel.csv')
-saveRDS(cariboumodel, '~/Stage/Stage Canada/Caribou data/cariboumodel.Rds')
+saveRDS(allweather, '~/Documents/Emilie_project/Git/emilie_nlcaribou/output/allweather2.Rds')
 
