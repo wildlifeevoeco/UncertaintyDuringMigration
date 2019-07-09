81368f0ff1d094ecba0bb3c7e6fac52ac9ec516b
Emilie Dedeban <ededeban@mun.ca>
2019-07-09 10:51:02 -0230

    update
    Former-commit-id: 6fe1f2288a3da6a3630a77feb9a5cb32cac29b75

--- scripts/Randomextract.R
+++ scripts/Randomextract.R
@@ -5,6 +5,8 @@ library(raster)
 library(adehabitatHR)
 library(spatstat)
 library(maptools)
+library(data.table)
+library(rgdal)
 #devtools::install_github('robitalec/toast')
 library(toast)
 
@@ -155,50 +157,150 @@ saveRDS(DataSortMidR, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/Ra
 RSFMigrationMR$Randoms <- as.factor(RSFMigrationMR$Randoms)
 Randoms<-subset(RSFMigrationMRfinal, Randoms != "1")
 Observed<-subset(RSFMigrationMRfinal, Randoms == "1")
+saveRDS(Randoms, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/RandomPoints/Randoms.RDS")
+saveRDS(Observed, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/RandomPoints/Observed.RDS")
 
+
+numloc <- Observed %>%
+  group_by(Year)%>%
+  summarise(total.fixes = n())
 
 setDT(Randoms)
 setDT(Observed)
 
+nrdm <- nrow(Randoms)
+Random1 <- Randoms[1:250000]
+Random2 <- Randoms[250001: nrdm]
+Randoms$FixDate <- as.Date( as.character(Randoms$FixDate), "%Y-%m-%d")
+Random1 <- subset(Randoms, Year == '2010') 
+Random2 <- subset(Randoms, Year == '2011' & FixDate >= '2011-02-12' & FixDate <= '2011-03-31')
+Random3 <- subset(Randoms, Year == '2011' & FixDate >= '2011-04-01' & FixDate <= '2011-05-19')
+Random4 <- subset(Randoms, Year > '2011' & Year <= '2013')
+
+utm21N <- '+proj=utm +zone=21 ellps=WGS84'
+Random2 <- as.data.table(Random2)
+Random3 <- as.data.table(Random3)
+Random4 <- as.data.table(Random4)
 
 build_pt_asset(
-  DT = Randoms,
-  out = 'output/Randoms-emilie-nlcaribou',
+  DT = Random1,
+  out = 'output/Random1-emilie-nlcaribou',
   projection = utm21N,
-  id = 'ANIMAL_ID',
+  id = 'Animal_ID',
   coords = c('Easting', 'Northing'),
-  extra = setdiff(colnames(Randoms), c('ANIMAL_ID', 'Easting', 'Northing')))
+  extra = setdiff(colnames(Randoms), c('Animal_ID', 'Easting', 'Northing')))
+
+build_pt_asset(
+  DT = Random2,
+  out = 'output/Random2-emilie-nlcaribou',
+  projection = utm21N,
+  id = 'Animal_ID',
+  coords = c('Easting', 'Northing'),
+  extra = setdiff(colnames(Randoms), c('Animal_ID', 'Easting', 'Northing')))
+
+build_pt_asset(
+  DT = Random3,
+  out = 'output/Random3-emilie-nlcaribou',
+  projection = utm21N,
+  id = 'Animal_ID',
+  coords = c('Easting', 'Northing'),
+  extra = setdiff(colnames(Randoms), c('Animal_ID', 'Easting', 'Northing')))
+
+build_pt_asset(
+  DT = Random4,
+  out = 'output/Random4-emilie-nlcaribou',
+  projection = utm21N,
+  id = 'Animal_ID',
+  coords = c('Easting', 'Northing'),
+  extra = setdiff(colnames(Randoms), c('Animal_ID', 'Easting', 'Northing')))
 
 build_pt_asset(
   DT = Observed,
   out = 'output/Observed-emilie-nlcaribou',
   projection = utm21N,
-  id = 'ANIMAL_ID',
+  id = 'Animal_ID',
   coords = c('Easting', 'Northing'),
-  extra = setdiff(colnames(Observed), c('ANIMAL_ID', 'Easting', 'Northing')))
+  extra = setdiff(colnames(Observed), c('Animal_ID', 'Easting', 'Northing')))
 
+######Solve problem with disparition of 'angle' and 'angleRes' columns######
+###create new dataframe with new column from Observed data with : time and ID
+Obsall <- Observed[,TimeIDYear := paste(Time, IDYear, sep = '_')]
+Obsall = subset(Obsall, select = c(9:10,21,23,27))
+##create new column from Daymet with time and ID
+observed_daymet_2<-as.data.table(observed_daymet_2)
+obsDaymet <- observed_daymet_2[,TimeIDYear := paste(Time, IDYear, sep = '_')]
+##Merge new dataset with obsDaymet by time/ID column
+mergeObs <- merge(Obsall, obsDaymet, all.x = TRUE, by = colnames("TimeIDYear"))
 
+###After EE
+obsDaymet <- fread('output/Observed-emilie-nlcaribou/observed-daymet.csv')
+rdm1Daymet <- fread('~/Documents/Emilie_project/Git/emilie_nlcaribou/output/Random1-emilie-nlcaribou/random1-daymet.csv')
+rdm2Daymet <- fread('~/Documents/Emilie_project/Git/emilie_nlcaribou/output/Random2-emilie-nlcaribou/random2-daymet-2.csv')
+rdm3Daymet <- fread('~/Documents/Emilie_project/Git/emilie_nlcaribou/output/Random3-emilie-nlcaribou/random3-daymet.csv')
+rdm4Daymet <- fread('~/Documents/Emilie_project/Git/emilie_nlcaribou/output/Random4-emilie-nlcaribou/random4-daymet.csv')
 
-saveRDS(Randoms, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/RandomPoints/Randoms.RDS")
-saveRDS(Observed, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/RandomPoints/Observed.RDS")
+rdm3Daymet = subset(rdm3Daymet, select = -c(20:21))
+rdmDaymet <- rbindlist(list(rdm1Daymet, rdm2Daymet, rdm3Daymet, rdm4Daymet))
+
+#Observed <- readRDS('the original Observed file')
+#Randoms <- readRDS('the original Randoms file')
+
+names(Randoms)[25]<-"ptID_1"
+Rdm = subset(rdmDaymet, select = c(23:26, 30:35))
+##Merge new dataset with obsDaymet by time/ID column
+mergeRdm <- merge(Rdm, Randoms, all.x = TRUE, by = colnames("ptID_1"))
+
 
 ####When I will extract covariates
 ### Next steps are to remove points where IRG or other variables are NA
+sum(is.na(mergeRdm$swe))
+sum(is.na(mergeRdm$tmax))
+sum(is.na(mergeRdm$tmin))     ### O NAs all covariates
+sum(is.na(mergeRdm$vp))
+sum(is.na(mergeRdm$prcp))
 
-Migration2<-Migration[!is.na(Migration$IRG),]
-nrow(Migration2)
-summary(Migration2)
-Migration2<-Migration2[!is.na(Migration2$PeakIRG),]
-Migration2<-Migration2[!is.na(Migration2$SnowOffDate),]
+saveRDS(mergeObs, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/mergeObs.RDS")
+saveRDS(mergeRdm, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/mergeRdm.RDS")
 
-setDT(Migration2)
+names(mergeObs)[7]<-"Animal_ID"
+names(mergeObs)[15]<-"JDateTime"
+names(mergeObs)[8]<-"Displace"
+names(mergeObs)[17]<-"MigStartDay"
+names(mergeObs)[16]<-"MigEndDay"
+names(mergeObs)[18]<-"MigDuration"
+setcolorder(mergeObs, c("Animal_ID","IDYear","Herd","FixDate","FixTime","Year","Month","JDate","Easting",
+                        "Northing","PtID","Time","JDateTime","NSD","Displace","MigStartDay","MigEndDay",
+                        "MigDuration", "state", "step", "angle", "stepRes", "angleRes", "Randoms", "IsWater", 
+                        "ptID_1", "jul", "prcp", "srad", "swe", "tmax", "tmin", "vp", "yr", ".geo"))
+mergeObs = subset(mergeObs, select = -c(36:40))
+setcolorder(mergeRdm, c("Animal_ID","IDYear","Herd","FixDate","FixTime","Year","Month","JDate","Easting",
+                        "Northing","PtID","Time","JDateTime","NSD","Displace","MigStartDay","MigEndDay",
+                        "MigDuration", "state", "step", "angle", "stepRes", "angleRes", "Randoms", "IsWater", 
+                        "ptID_1", "jul", "prcp", "srad", "swe", "tmax", "tmin", "vp", "yr", ".geo"))
 
-NewD<-data.table(Migration2)
+mergeObs<-mergeObs[order(mergeObs$ptID_1),]
+mergeRdm<-mergeRdm[order(mergeRdm$ptID_1),]
+
+mergeObs$FixDate <-as.Date(mergeObs$FixDate, format="%Y-%m-%d")
+mergeObs$FixTime <-as.ITime(mergeObs$FixTime, format="%H:%M:%S")
+mergeRdm$FixDate <-as.Date(mergeRdm$FixDate, format="%Y-%m-%d")
+mergeRdm$FixTime <-as.ITime(mergeRdm$FixTime, format="%H:%M:%S")
+mergeRdm$Time <- as.POSIXct(mergeRdm$Time,format="%Y-%m-%d %H:%M:%S")
+mergeObs$Time <- as.POSIXct(mergeObs$Time,format="%Y-%m-%d %H:%M:%S")
+
+allweather <- rbind(mergeObs,mergeRdm)
+saveRDS(allweather, "~/Documents/Emilie_project/Git/emilie_nlcaribou/output/allweather.Rds")
+
+#Migration2<-Migration[!is.na(mergeObs$swe),]
+#nrow(Migration2)
+#summary(Migration2)
+#Migration2<-Migration2[!is.na(Migration2$PeakIRG),]
+#Migration2<-Migration2[!is.na(Migration2$SnowOffDate),]
+#setDT(Migration2)
+#NewD<-data.table(Migration2)
 
 #### Find points/strata where only random points exist (mean == 0)
-NewD[, StrMean := mean(Pres),by = .(strata)]
-summary(NewD)
-nrow(NewD)
-
-
+#NewD[, StrMean := mean(Pres),by = .(strata)]
+#summary(NewD)
+#nrow(NewD)
 
