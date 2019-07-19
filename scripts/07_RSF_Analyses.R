3ba6b9e88cfc45cb9612a6ccee7cfe4a08220053
Emilie Dedeban <ededeban@mun.ca>
2019-07-19 20:48:42 -0230

    reorg script
    Former-commit-id: cbe191631b00c41f0ac250b7c42f1c7b8615cfc0

--- scripts/07_RSF_Analyses.R
+++ scripts/07_RSF_Analyses.R
@@ -3,21 +3,36 @@
 ####################################################
 library(mclogit)
 library(lme4)
+library(car)
+library(Rmisc)
+library(ggplot2)
+library(jtools)
+library(MuMIn)
+library(data.table)
 
+#####Arranging data and graphs exploration#######
+####Changing name of available and used pts to easily interpret them after
 allNDVI$Randoms<-ifelse(allNDVI$Randoms=="1",1,0) #### 0 == available and 1 == used 
 head(allNDVI)
 summary(allNDVI)
+
+###add column to reorganise Randoms to see if there is some of
+### randoms points who not have used pts (removed from water step)
 allNDVI[, StrMean := mean(as.numeric(Randoms)),by = .(PtID)]
 summary(allNDVI)
 nrow(allNDVI)
-###subset by state
+
+###changing name of state variables for interpretation and subset
 allNDVI$HMM <- ifelse(allNDVI$state == 2,0,1)
 allNDVI_stop<- subset(allNDVI, HMM == 1)
 allNDVI_mvt <- subset(allNDVI, HMM == 0)
 
+
+###Calculate Se within variables (here NDVI)
 NDVIrsf <- summarySEwithin(allNDVI_stop, measurevar = "NDVI", idvar = "Animal_ID",
                         withinvars = "Randoms", "Year", na.rm = FALSE, conf.interval = .95)
 
+##Plot SE of NDVI
 NDVIrsfyear<-ggplot(NDVIrsf, aes(x=Year, y=NDVI, colour=Randoms, group = Randoms)) +
   geom_line() +
   geom_errorbar(width=0.2, aes(ymin=NDVI-sd, ymax=NDVI+sd))+
@@ -25,25 +40,51 @@ NDVIrsfyear<-ggplot(NDVIrsf, aes(x=Year, y=NDVI, colour=Randoms, group = Randoms
   ylim(-0.25,0.5)+
   theme_bw()
 
-###summary temperature 
+###Calculate mean and se within variables (here temperature) 
 tmaxrsf <- summarySEwithin(allNDVI_stop, measurevar = "tmax", idvar = "Animal_ID",
                            withinvars = "Randoms", "Year", na.rm = FALSE, conf.interval = .95)
-####SE variation between years
+####Plot SE variation of temp between years
 tmaxrsfyear<-ggplot(tmaxrsf, aes(x=Year, y=tmax, colour=Randoms, group = Randoms)) +
   geom_line() +
   geom_errorbar(width=0.2, aes(ymin=tmax-sd, ymax=tmax+sd)) +
   geom_point(shape=20, size=3, fill="white") +
   theme_bw()
 
+###############################################
+#######RSF MODEL WITH ENCAMPED#################
+###Habitat model RSF without year and indiv
+modelhab1 <- glm(Randoms ~ scale(Wetland) + scale(Forest) + scale(Lichen) + scale(Water) + scale(Rocky) , data = allNDVI_stop, family = "binomial")
 
-#######RSF MODEL WITH ENCAMPED
-###Habitat model RSF
-model1hab<-glmer(Randoms ~ scale(Wetland) + scale(Forest) + scale(Lichen) + scale(Water) + scale(Rocky) + (1|Animal_ID) + (1|Year), data = allNDVI_stop, family = "binomial")
-summary(model1hab)
+summ(modelhab1)
+
+car::vif(modelhab1)
+
+#####Habitat model RSF with indiv and year as a random effects
+modelhab2<-glmer(Randoms ~ scale(Wetland) + scale(Forest) + scale(Lichen) + scale(Water) + scale(Rocky) + (1|Animal_ID) + (1|Year), data = allNDVI_stop, family = "binomial")
+summ(modelhab2)
+
 boxplot((Lichen) ~ factor(Randoms)  , data = allNDVI_stop, notch = TRUE)
 
-summ(model1hab)
-###NDVI model
+###Let's try habitat model with Year as fixed effect
+PROBLEM
+
+
+###excluding water 
+modelhab3<-glmer(Randoms ~ scale(Wetland) + scale(Forest) + scale(Lichen) + scale(Rocky) + (1|Animal_ID) + (1|Year), data = allNDVI_stop, family = "binomial")
+summ(modelhab3)
+
+###Foraging habitat
+modelhab4 <- glmer (Randoms ~ scale(Wetland) + scale(Lichen) + (1|Animal_ID) + (1|Year), data = allNDVI_stop, family = "binomial")
+summ(modelhab4)
+
+####R squared 
+r.squaredGLMM(modelhab1)
+r.squaredGLMM(modelhab2)
+r.squaredGLMM(modelhab3)
+r.squaredGLMM(modelhab4)
+
+
+###NDVI model with mclogit
 RSFmodelNDVI<-mclogit(cbind(Randoms,PtID)~scale(NDVI), data = allNDVI_stop)
 summary(RSFmodelNDVI)
 boxplot((NDVI) ~ factor(Randoms)  , data = allNDVI_stop, notch = TRUE)
